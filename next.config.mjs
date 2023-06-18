524b84d4d3e6d079e6ea8b66da3c4039d973e749
Tim Pechersky <tim.pechersky@gmail.com>
2023-06-18 13:13:58 +0800

    remove pages action from workflow

--- next.config.mjs
+++ next.config.mjs
@@ -3,39 +3,42 @@ import nextMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 /** @type {import('next').NextConfig} */
 const withMDX = nextMDX({
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [emoji, remarkGfm],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
-  },
+    options: {
+        // If you use remark-gfm, you'll need to use next.config.mjs
+        // as the package is ESM only
+        // https://github.com/remarkjs/remark-gfm#install
+        remarkPlugins: [emoji, remarkGfm],
+        rehypePlugins: [],
+        // If you use `MDXProvider`, uncomment the following line.
+        providerImportSource: "@mdx-js/react",
+    },
 });
 export default withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  reactStrictMode: false,
-  trailingSlash: true,
-  // output: "standalone",
-  // target: "serverless",
-  webpack: (config, { isServer, webpack }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      // config.node = { fs: 'empty' };
-      config.resolve.fallback.fs = false;
-      config.resolve.fallback.electron = false;
-    }
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^electron$/,
-      })
-    );
+    images: { unoptimized: true },
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    reactStrictMode: false,
+    trailingSlash: true,
+    // output: "standalone",
+    // target: "serverless",
+    webpack: (config, { isServer, webpack }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+            // config.node = { fs: 'empty' };
+            config.resolve.fallback.fs = false;
+            config.resolve.fallback.electron = false;
+        }
+        config.plugins.push(
+            new webpack.IgnorePlugin({
+                resourceRegExp: /^electron$/,
+            })
+        );
 
-    return config;
-  },
-  experimental: {
-    externalDir: true,
-    esmExternals: false,
-  },
+        return config;
+    },
+    experimental: {
+        externalDir: true,
+        esmExternals: false,
+        images: { unoptimized: true },
+    },
+    basePath: "",
 });
